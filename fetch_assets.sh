6d32aec95285017ccf0b4e61c8d5426512e49170
Simon Venzlaff <simon.venzlaff@student.hpi.de>
2025-04-30 18:29:32 +0200

    Move files to external fileserver

---
+++ fetch_assets.sh
@@ -0,0 +1,4 @@
+curl -u "x2AJL3ezgeCkmWF:cj3cq7TYWP" --header "X-Requested-With: XMLHttpRequest" "https://nextcloud.hpi.de/public.php/webdav/" > villevite/Assets.zip
+cd villevite
+unzip Assets.zip
+rm Assets.zip
