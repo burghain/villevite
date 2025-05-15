4ef9fc7d65197191995f1fcd2335d9b8c007e45f
bigly4 <josua@zehner.eu>
2025-05-15 16:41:50 +0200

    Fixed tests and added submodule initialization to ci

--- tests/test_assets.py
+++ tests/test_assets.py
@@ -10,7 +10,7 @@ def test_assets_import_all(import_assets):
 
 def test_assets_import_all_twice(import_assets):
     duplicate_existed_before = "City Generator.001" in bpy.data.node_groups
-    assets.import_assets()
+    assets.import_assets_and_nodes()
     assert "City Generator" in bpy.data.node_groups
     assert duplicate_existed_before == (
         "City Generator.001" in bpy.data.node_groups)
